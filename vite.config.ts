25d69492bacd4a2b4ca4b612f230d31c829507cd
NeOpoku <imanimoss17@gmail.com>
2025-04-17 19:35:31 -0400

    added a new plugin to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './',
   plugins: [react()],
+  preview: {
+    host: true,                        // listen on 0.0.0.0
+    port: Number(),    // use the Render-assigned port
+    allowedHosts: [
+      'module-13-bbw8.onrender.com'    // add your Render hostname here
+    ]
+  }
 });
